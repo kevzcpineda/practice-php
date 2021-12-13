8488e8d37dd173bd7666ee8c48c67d07a36d8673
kevzcpineda <kevinpineda098@gmail.com>
2021-12-13 11:43:28 +0800

    update

--- test_db.php
+++ test_db.php
@@ -4,7 +4,7 @@
     $db = "fx4_db";
     $dsn ="/cloudsql/inventory-333214:asia-southeast1:inventoryapp";
     // $dsn ="34.124.166.104";
-    $con = mysqli_connect(null,$username,$password,$db,null,$dsn);
-
+    // $con = mysqli_connect(null,$username,$password,$db,null,$dsn);
+    $con = new mysqli($dsn,$username,$password,$db);
     echo"kevin";
 ?>
